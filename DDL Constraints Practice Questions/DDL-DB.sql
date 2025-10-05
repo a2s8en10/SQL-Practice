1f4f6de7a5edb94b200fc61c2d287726bb8908da
Anurag Sahu <anu.sahu9589@gmail.com>
2025-10-06 00:23:49 +0530

    Create DataBase

--- DDL Constraints Practice Questions/DDL-DB.sql
+++ DDL Constraints Practice Questions/DDL-DB.sql
@@ -2,10 +2,3 @@ USE ShopDB;
 
 SELECT * FROM Customer;
 SELECT * FROM Orders;
-
---  ## how to create a view
--- CREATE VIEW CustomerOrders AS
--- SELECT FullName, Age , City
--- FROM Customer
--- -- WHERE Age > 25;
--- SELECT * FROM CustomerOrders;
